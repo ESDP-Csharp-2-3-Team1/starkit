e39ac3d618cab603f4d5dcbfd78d2fee717422d9
Samat Murat <muratsamat090598@gmail.com>
2020-07-25 14:28:34 +0600

    #3 Добавил модель категорий, и добавил к нему миграцию

---
+++ Starkit/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Starkit.Models
+{
+    public class Category
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Name { get; set; }
+    }
+}
--- Starkit/Models/Data/StarkitContext.cs
+++ Starkit/Models/Data/StarkitContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +7,21 @@ namespace Starkit.Models.Data
 {
     public class StarkitContext : IdentityDbContext<User>
     {
+        public DbSet<Category> Categories { get; set; }
         
+        public StarkitContext(DbContextOptions<StarkitContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Category>().HasData(
+                new Category[]
+                {
+                    new Category{Id = Guid.NewGuid().ToString(), Name = "Первые блюда"}, 
+                    new Category{Id = Guid.NewGuid().ToString(), Name = "Вторые блюда"}, 
+                    new Category{Id = Guid.NewGuid().ToString(), Name = "Десерты"}, 
+                    new Category{Id = Guid.NewGuid().ToString(), Name = "Напитки"} 
+                });
+        }
     }
 }
