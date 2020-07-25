e465438d58ecc20fc25f974be7775a2a4996dabf
Almat <seyderaly98@gmail.com>
2020-07-25 18:06:01 +0600

    #1 Добавил model ресторатора

---
+++ Starkit/Models/LegalAddress.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Starkit.Models
+{
+    public class LegalAddress
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поля обязательные для заполнения")]
+        public int Index { get; set; }
+        public string Country { get; set; } = "Казахстан";
+        [Required(ErrorMessage = "Поля обязательные для заполнения")]
+        public string Region { get; set; }
+        [Required(ErrorMessage = "Поля обязательные для заполнения")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Поля обязательные для заполнения")]
+        public string Address { get; set; }
+
+        public string UserId { get; set; }
+    }
+
+}
--- Starkit/Models/User.cs
+++ Starkit/Models/User.cs
@@ -4,6 +4,10 @@ namespace Starkit.Models
 {
     public class User : IdentityUser
     {
-        
+        public string Login { get; set; }
+        public string SurName { get; set; }
+        public string CompanyName { get; set; }
+        public int IIN { get; set; }
+        public string CityPhone { get; set; }
     }
 }
