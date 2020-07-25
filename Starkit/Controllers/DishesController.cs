1517d0d0e4720cc8d6dcc41bac6d62634f2db00a
Samat Murat <muratsamat090598@gmail.com>
2020-07-25 17:08:40 +0600

    #3 Реализовал логику добавления данных блюд на базу данных

---
+++ Starkit/Controllers/DishesController.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Starkit.Models;
+using Starkit.Models.Data;
+using Starkit.Services;
+
+namespace Starkit.Controllers
+{
+    public class DishesController : Controller
+    {
+        private StarkitContext _db;
+        private IHostEnvironment _environment;
+        private UploadService _uploadService;
+
+        public DishesController(StarkitContext db, IHostEnvironment environment, UploadService uploadService)
+        {
+            _db = db;
+            _environment = environment;
+            _uploadService = uploadService;
+        }
+
+        private string Load(string id, IFormFile file)
+        {
+            string path = Path.Combine(_environment.ContentRootPath + $"\\wwwroot\\images\\Dishes\\{id}");
+            string photoPath = $"images/Dishes/{id}/{file.FileName}";
+            if (!Directory.Exists($"wwwroot/images/Dishes/{id}"))
+            {
+                Directory.CreateDirectory($"wwwroot/images/Dishes/{id}");
+            }
+            _uploadService.Upload(path, file.FileName, file);
+            return photoPath;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Dish dish = new Dish{Categories = _db.Categories.ToList()};
+            return View(dish);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(Dish dish)
+        {
+            if (ModelState.IsValid)
+            {
+                dish.Avatar = Load(dish.Id, dish.File);
+                _db.Entry(dish).State = EntityState.Added;
+                await _db.SaveChangesAsync();
+            }
+            return View(dish);
+        }
+    }
+}
