d6ae744895a6445ffdc206cb30d52636428d226a
Samat Murat <muratsamat090598@gmail.com>
2020-07-26 21:11:37 +0600

    #5 Реализовал модальное окно для утверждения запроса

--- Starkit/wwwroot/js/site.js
+++ Starkit/wwwroot/js/site.js
@@ -1,17 +1,16 @@
 ﻿function deleteDishes() {
-    var checkboxes = $("#checkbox-td input:checkbox:checked");
+    var checkboxes = $("input:checkbox:checked");
     let arr = [];
     $.each(checkboxes, function (index, value) {
        arr.push(value.id);
     });
-    if (arr.length > 0){
         $.ajax({
             url: "http://localhost:5000/Dishes/Delete",
             type: "Delete",
             data: {ids:arr},
             success: function (data) {
                 $("#result").html(data);
+                $(".show").hide();
             }
-        })   
-    }
+        })
 }
