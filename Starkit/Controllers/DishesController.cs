542df38113ead7f19226ead149f001bce7f6a364
Samat Murat <muratsamat090598@gmail.com>
2020-07-26 17:53:14 +0600

    #4 Реализовал логику редактирования данных блюда

--- Starkit/Controllers/DishesController.cs
+++ Starkit/Controllers/DishesController.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Starkit.Models;
 using Starkit.Models.Data;
 using Starkit.Services;
+using Starkit.ViewModels;
 
 namespace Starkit.Controllers
 {
@@ -26,6 +28,15 @@ namespace Starkit.Controllers
             _uploadService = uploadService;
             _userManager = userManager;
         }
+        
+        private void DeleteDishAvatar(Dish dish)
+        {
+            string filePath = _environment.ContentRootPath + "\\wwwroot\\images\\Dishes\\" + dish.Id; 
+            if (Directory.Exists(filePath))
+            {
+                System.IO.File.Delete("wwwroot/" + dish.Avatar);
+            }
+        }
 
         private string Load(string id, IFormFile file)
         {
@@ -39,6 +50,7 @@ namespace Starkit.Controllers
             return photoPath;
         }
 
+        [Authorize]
         [HttpGet]
         public IActionResult Create()
         {
@@ -58,6 +70,49 @@ namespace Starkit.Controllers
                 return RedirectToAction("Create");
             }
             return View(dish);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            Dish dish = _db.Dishes.FirstOrDefault(d => d.Id == id);
+            EditDishViewModel model = new EditDishViewModel
+            {
+                Id = id,
+                Category = dish.Category,
+                Name = dish.Name,
+                Cost = dish.Cost,
+                Description = dish.Description,
+                Calorie = dish.Calorie,
+                ProperNutrition = dish.ProperNutrition,
+                Vegetarian = dish.Vegetarian,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditDishViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Dish dish = _db.Dishes.FirstOrDefault(d => d.Id == model.Id);
+                dish.Name = model.Name;
+                dish.Cost = model.Cost;
+                dish.Description = model.Description;
+                dish.Calorie = model.Calorie;
+                dish.ProperNutrition = model.ProperNutrition;
+                dish.Vegetarian = model.Vegetarian;
+                if (model.File != null)
+                {
+                    DeleteDishAvatar(dish);
+                    dish.Avatar = Load(model.Id, model.File);
+                }
+                _db.Entry(dish).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index", "Menu");
+            }
+            return View(model);
         }
     }
 }
---
+++ Starkit/ViewModels/EditDishViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+using Starkit.Models;
+
+namespace Starkit.ViewModels
+{
+    public class EditDishViewModel
+    {
+        public string Id { get; set; }
+        public virtual Category Category { get; set; }
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public decimal Cost { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Description { get; set; }
+
+        public IFormFile File { get; set; }
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public double Calorie { get; set; }
+
+        public bool ProperNutrition { get; set; }
+        
+        public bool Vegetarian { get; set; }
+    }
+}
