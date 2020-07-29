fb48f5cd059e3bfe6eda894238e1222c7611302b
Samat Murat <muratsamat090598@gmail.com>
2020-07-29 17:34:20 +0600

    #4 Добавил модель для меню

---
+++ Starkit/Models/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Starkit.Models
+{
+    public class Menu
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        
+        public string CreatorId { get; set; }
+        public virtual User Creator { get; set; }
+        
+        public string EditorId { get; set; }
+        public virtual User Editor { get; set; }
+
+        public DateTime AddTime { get; set; }
+        public DateTime? EditTime { get; set; }
+
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Name { get; set; }
+        
+        [NotMapped]
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public IFormFile File { get; set; }
+
+        public string Avatar { get; set; }
+        
+        public virtual List<MenuDish> MenuDishes  { get; set; }
+        
+        [NotMapped] 
+        public IEnumerable<IGrouping<Category, Dish>> Dishes { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public List<string> DishesId { get; set; }
+    }
+}
