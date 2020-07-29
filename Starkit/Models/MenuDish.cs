7df14c1b0e169e7fa18586d2f75e673426ee54ba
Samat Murat <muratsamat090598@gmail.com>
2020-07-29 17:35:12 +0600

    #4 Добавил модель MenuDish.cs для связи многие ко многим между таблицами Dishes и Menu

---
+++ Starkit/Models/MenuDish.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Starkit.Models
+{
+    public class MenuDish
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        
+        public string MenuId { get; set; }
+        public virtual Menu Menu { get; set; }
+
+        public string DishId { get; set; }
+        public virtual Dish Dish { get; set; }
+    }
+}
