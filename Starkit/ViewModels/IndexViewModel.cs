2d0c8ddc4dd5be84055af7bfe91c710a0a2b1ba2
Samat Murat <muratsamat090598@gmail.com>
2020-07-30 13:10:07 +0600

    #7 Добавил модель IndexViewModel.cs для объединение фильтраций, пагинаций и сортировки

---
+++ Starkit/ViewModels/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Starkit.Models;
+
+namespace Starkit.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Dish> Dishes { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+        public FilterViewModel FilterViewModel { get; set; }
+        public SortViewModel SortViewModel { get; set; }
+    }
+}
