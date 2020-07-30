816bac4fcf802afcf1d4ceca8f167b27825581ed
Samat Murat <muratsamat090598@gmail.com>
2020-07-30 13:08:34 +0600

    #7 Добавил модель FilterViewModel.cs для фильтраций

---
+++ Starkit/ViewModels/FilterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Starkit.Models;
+
+namespace Starkit.ViewModels
+{
+    public class FilterViewModel
+    {
+        public FilterViewModel(List<Category> categories, string category, string name)
+        {
+            categories.Insert(0, new Category{Name = "Все", Id = null});
+            Categories = new SelectList(categories, "Id", "Name", category);
+            SelectedCategory = category;
+            SelectedName = name;
+        }
+        
+        public SelectList Categories { get; set; }
+        public string SelectedCategory { get; set; }
+        public string SelectedName { get; set; }
+    }
+}
