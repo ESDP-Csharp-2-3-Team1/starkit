39577ac519464c96974d64021ecc217a8a180aaa
Samat Murat <muratsamat090598@gmail.com>
2020-07-30 13:09:16 +0600

    #7 Добавил модель SortViewModel.cs для сортировки

---
+++ Starkit/ViewModels/SortViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Starkit.ViewModels
+{
+    public enum SortState
+    {
+        NameAsc,
+        NameDesc,
+        CostAsc,
+        CostDesc,
+        AddTimeAsc,
+        AddTimeDesc,
+        CategoryAsc,
+        CategoryDesc,
+        CalorieAsc,
+        CalorieDesc
+    }
+    public class SortViewModel
+    {
+        public SortState NameSort { get; set; }
+        public SortState CostSort { get; set; }
+        public SortState AddTimeSort { get; set; }
+        public SortState CategorySort { get; set; }
+        public SortState CalorieSort { get; set; }
+        public SortState Current { get; set; }
+
+        public SortViewModel(SortState sortOrder)
+        {
+            NameSort = sortOrder == SortState.NameAsc ? SortState.NameDesc : SortState.NameAsc;
+            CostSort = sortOrder == SortState.CostAsc ? SortState.CostDesc : SortState.CostAsc;
+            AddTimeSort = sortOrder == SortState.AddTimeAsc ? SortState.AddTimeDesc : SortState.AddTimeAsc;
+            CategorySort = sortOrder == SortState.CategoryAsc ? SortState.CategoryDesc : SortState.CategoryAsc;
+            CalorieSort = sortOrder == SortState.CalorieAsc ? SortState.CalorieDesc : SortState.CalorieAsc;
+            Current = sortOrder;
+        }
+    }
+}
