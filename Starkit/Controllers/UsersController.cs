f07ffe2dc18de71a5cb7409d11f251c93d6d6bdd
Almat <seyderaly98@gmail.com>
2020-08-13 12:29:26 +0600

    #31 Добавлен ation UpdateUserInformation

--- Starkit/Controllers/UsersController.cs
+++ Starkit/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Starkit.Models;
 using Starkit.Models.Data;
 using Starkit.ViewModels;
@@ -37,6 +38,35 @@ namespace Starkit.Controllers
             ViewBag.PostalAddress = _db.PostalAddresses.FirstOrDefault(p => p.UserId == userId);
             ViewBag.User = await _userManager.FindByIdAsync(userId);
             return View(model);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> UpdateUserInformation(EditUserViewModel model)
+        {
+            if (model != null)
+            {
+                string userId = _userManager.GetUserId(User);
+                LegalAddress legalAddress = await _db.LegalAddresses.FirstOrDefaultAsync(l => l.UserId == userId);
+                PostalAddress postalAddress = await _db.PostalAddresses.FirstOrDefaultAsync(p => p.UserId == userId);
+                if (legalAddress is null || postalAddress is null)
+                    return NotFound();
+                legalAddress.City = model.LegalAddress.City;
+                legalAddress.Region = model.LegalAddress.Region;
+                postalAddress.City = model.PostalAddress.City;
+                postalAddress.Region = model.PostalAddress.Region;
+                if (!string.IsNullOrEmpty(model.CityPhone))
+                {
+                    User user = await _db.Users.FirstOrDefaultAsync(u => u.Id == userId);
+                    user.CityPhone = model.CityPhone;
+                    user.CityPhone = model.CityPhone;
+                    _db.Users.Update(user);
+                }
+                _db.LegalAddresses.Update(legalAddress);
+                _db.PostalAddresses.Update(postalAddress);
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
         }
     }
 }
