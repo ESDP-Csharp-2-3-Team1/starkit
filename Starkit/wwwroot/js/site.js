3bc448120612e0f33d53bcf35aee41dd1822d1da
Almat <seyderaly98@gmail.com>
2020-08-13 14:42:55 +0600

    #22 При клике на ссылку в меню навигации  появляются вложенные ссылки, основные ссылки находящиеся после открытой смещаются ниже на высоту открытой вкладки с вложенными ссылками.

--- Starkit/wwwroot/js/site.js
+++ Starkit/wwwroot/js/site.js
@@ -1,1 +1,8 @@
 ﻿
+function showDropdown(value){
+    let el;
+    if (value === 1){
+        el = document.getElementById('dropdown1');
+        el.style.display === 'none' ? el.style.display = 'initial' : el.style.display = 'none';
+    }
+}
