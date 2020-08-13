651c5444836e2f5e9304ea5ecc29ba008a111eee
Almat <seyderaly98@gmail.com>
2020-08-13 15:00:01 +0600

    #22 Добавил dropdown меню №2

--- Starkit/wwwroot/js/site.js
+++ Starkit/wwwroot/js/site.js
@@ -2,7 +2,13 @@
 function showDropdown(value){
     let el;
     if (value === 1){
+        $('#dropdown2').hide();
         el = document.getElementById('dropdown1');
-        el.style.display === 'none' ? el.style.display = 'initial' : el.style.display = 'none';
     }
+    else if (value === 2){
+        $('#dropdown1').hide();
+        el = document.getElementById('dropdown2');
+    }
+    el.style.display === 'none' ? el.style.display = 'initial' : el.style.display = 'none';
+
 }
