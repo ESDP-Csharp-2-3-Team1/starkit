601f06dc1f3da49bc862c0364e580f208d320365
Almat <seyderaly98@gmail.com>
2020-08-13 23:56:41 +0600

    #29 Исправлена задержка навигационного бара при клике

--- Starkit/wwwroot/js/site.js
+++ Starkit/wwwroot/js/site.js
@@ -1,5 +1,6 @@
 ﻿
 function showDropdown(value){
+    console.log(value)
     let el;
     if (value === 1){
         $('#dropdown2').hide();
@@ -9,6 +10,9 @@ function showDropdown(value){
         $('#dropdown1').hide();
         el = document.getElementById('dropdown2');
     }
+    console.log(el.style.display)
+    if (el.style.display === '') 
+        el.style.display = 'none';
+        
     el.style.display === 'none' ? el.style.display = 'initial' : el.style.display = 'none';
-
 }
