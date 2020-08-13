1df7c425af8d6d14fc3b1fa46968d00b351ee708
Almat <seyderaly98@gmail.com>
2020-08-13 16:50:09 +0600

    #29 Добавил класс Restaurant

---
+++ Starkit/Controllers/RestaurantsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Starkit.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        
+        
+        
+        // GET
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Starkit/Models/Restaurant.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+
+namespace Starkit.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string NameRestaurant { get; set; }
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string ContactPerson { get; set; }
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Address { get; set; }
+        public string RestaurantInformation { get; set; }
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public bool DomainAvailability { get; set; }
+        public string DomainName { get; set; }
+        public string LogoPath { get; set; }
+        [NotMapped]
+        public IFormFile File { get; set; }
+    }
+}
