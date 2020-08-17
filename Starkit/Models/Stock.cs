51670ead2c04e2113a47c02826fe37e7897898b6
Samat Murat <muratsamat090598@gmail.com>
2020-08-17 23:25:58 +0600

    #35 Добавил страницу для добавление акций

---
+++ Starkit/Models/Stock.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+using Org.BouncyCastle.Asn1.Cms;
+
+namespace Starkit.Models
+{
+    public class Stock
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Type { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Validity { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public DateTime At { get; set; }
+        
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public DateTime To { get; set; }
+        
+        public string Avatar { get; set; }
+        
+        [NotMapped]
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public IFormFile File { get; set; }
+        
+        public string CreatorId { get; set; }
+        public virtual User Creator { get; set; }
+        
+        public string EditorId { get; set; }
+        public virtual User Editor { get; set; }
+    }
+}
