69ca2da3a04d2dca34248879c770082549f2fa2c
Almat <seyderaly98@gmail.com>
2020-08-25 14:54:51 +0600

    Исправлен баг сущности User

--- Starkit/Models/User.cs
+++ Starkit/Models/User.cs
@@ -19,8 +19,9 @@ namespace Starkit.Models
         public UserStatus Status { get; set; } = UserStatus.Unlocked;
         public bool IsTermsAccepted { get; set; }
         public string RestaurantId { get; set; }
+        [NotMapped]
         public Restaurant Restaurant { get; set; }
-
+        
         [NotMapped]
         public LegalAddress LegalAddress { get; set; }
         [NotMapped]
