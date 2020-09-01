5f3ca954634325d206a6a3fbabadea81e05183a5
Samat Murat <muratsamat090598@gmail.com>
2020-09-01 23:46:30 +0600

    #50 Добавил модальное окно для формы для оформления заказа

---
+++ Starkit/Controllers/OrdersController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Starkit.Models;
+using Starkit.Services;
+
+namespace Starkit.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Add()
+        {
+            List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+            if (cart == null)
+                cart = new List<Item>();
+            if (cart.Count != 0)
+                return PartialView("PartialViews/ToOrderModalWindowPartialView", new Order());
+            return Json(false);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Add(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                List<Item> cart = SessionHelper.GetObjectFromJson<List<Item>>(HttpContext.Session, "cart");
+                foreach (var item in cart)
+                {
+                    
+                }
+                return Json(false);   
+            }
+            return PartialView("PartialViews/ToOrderModalWindowPartialView", order);
+        }
+    }
+}
---
+++ Starkit/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Starkit.Models
+{
+    public class Order
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string ContactNumber { get; set; }
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        public string Address { get; set; }
+
+        public string RestaurantId { get; set; }
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
