8e061db623e3a31e2797951fb307aa48ae7b820d
Almat <seyderaly98@gmail.com>
2020-09-15 17:17:36 +0600

    #64 Добавлен SiteCardsController.cs, настроен начальный Layout сайта визитки, также добавлен новый стиль (styleSiteCard.css) взаймадействуеши искльючительно с сайтом визитки

---
+++ Starkit/Controllers/SiteCardsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Starkit.Controllers
+{
+    public class SiteCardsController : Controller
+    {
+        
+        
+        // GET
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
