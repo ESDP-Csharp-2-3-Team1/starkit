d7a51b59c66f4634d96d05b51adf447bc7eb1757
Samat Murat <muratsamat090598@gmail.com>
2020-09-24 14:15:37 +0600

    #66 Реализовал серверную часть получения популярных продуктов

--- Starkit/Controllers/SalesStaticsController.cs
+++ Starkit/Controllers/SalesStaticsController.cs
@@ -64,5 +64,46 @@ namespace Starkit.Controllers
             }
             return Json(orders.OrderBy(o => o.OrderTime));
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPopularGoods()
+        {
+            User user = await _userManager.GetUserAsync(User);
+            Restaurant restaurant = _db.Restaurants.FirstOrDefault(r => r.Id == user.RestaurantId);
+            List<Order> orders = restaurant.Orders.Where(o => o.Status != Status.Отказ && o.Status != Status.Новая)
+                .ToList();
+            IEnumerable<OrderProduct> dishes = orders.Where(o => o.OrdersProducts != null)
+                .SelectMany(o => o.OrdersProducts.Where(op => op.Dish != null));
+            var popularDish = dishes.GroupBy(d => d.Dish.Name)
+                .Select(g => new
+                {
+                    Name = g.First().Dish.Name,
+                    Qantity = g.Sum(s => s.Quantity),
+                    Sum = g.Sum(s => s.Quantity) * g.First().Dish.Cost
+                }).OrderByDescending(d => d.Qantity).First();
+            IEnumerable<OrderProduct> menu = orders.Where(o => o.OrdersProducts != null)
+                .SelectMany(o => o.OrdersProducts.Where(op => op.Menu != null));
+            var popularMenu = menu.GroupBy(d => d.Menu.Name)
+                .Select(g => new
+                {
+                    Name = g.First().Menu.Name,
+                    Qantity = g.Sum(s => s.Quantity),
+                    Sum = g.Sum(s => s.Quantity) * g.First().Menu.Cost
+                }).OrderByDescending(m => m.Qantity).First();
+            IEnumerable<OrderProduct> stocks = orders.Where(o => o.OrdersProducts != null)
+                .SelectMany(o => o.OrdersProducts.Where(op => op.Stock != null));
+            var popularStock = stocks.GroupBy(d => d.Stock.Name)
+                .Select(g => new
+                {
+                    Name = g.First().Stock.Name,
+                    Qantity = g.Sum(s => s.Quantity),
+                    Sum = g.Sum(s => s.Quantity) * g.First().Stock.Cost
+                }).OrderByDescending(s => s.Qantity).First();
+            List<Object> popularProducts = new List<object>();
+            popularProducts.Add(popularDish);
+            popularProducts.Add(popularMenu);
+            popularProducts.Add(popularStock);
+            return Json(true);
+        }
     }
 }
