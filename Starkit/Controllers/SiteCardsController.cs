b8f3520d7794ee26f8f61f8369d60e9833d45bfc
Almat <seyderaly98@gmail.com>
2020-09-29 16:27:44 +0600

    #64 Добавлены некоторые информация о ресторане

--- Starkit/Controllers/SiteCardsController.cs
+++ Starkit/Controllers/SiteCardsController.cs
@@ -1,14 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Starkit.Models;
+using Starkit.Models.Data;
 
 namespace Starkit.Controllers
 {
     public class SiteCardsController : Controller
     {
-        
-        
-        // GET
-        public IActionResult Index()
+        private StarkitContext _db { get; set; }
+        private UserManager<User> _userManager { get; set; }
+
+        public SiteCardsController(StarkitContext db, UserManager<User> userManager)
         {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var userId = _userManager.GetUserId(User);
+            ViewBag.Restaurant = await _db.Restaurants.FirstOrDefaultAsync(r => r.UserId == userId);
             return View();
         }
     }
