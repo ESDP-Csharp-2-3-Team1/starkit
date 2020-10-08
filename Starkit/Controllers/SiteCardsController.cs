9313b86ad7e5aa368cf77ce8c6bf299500bc07d4
Almat <seyderaly98@gmail.com>
2020-10-08 18:45:25 +0600

    #65 Реализован функционал смены изображений каруселья

--- Starkit/Controllers/SiteCardsController.cs
+++ Starkit/Controllers/SiteCardsController.cs
@@ -1,11 +1,15 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Starkit.Models;
 using Starkit.Models.Data;
+using Starkit.Services;
 
 namespace Starkit.Controllers
 {
@@ -13,11 +17,15 @@ namespace Starkit.Controllers
     {
         private StarkitContext _db { get; set; }
         private UserManager<User> _userManager { get; set; }
+        private IHostEnvironment _environment { get; set; }
+        private UploadService _uploadService { get; set; }
 
-        public SiteCardsController(StarkitContext db, UserManager<User> userManager)
+        public SiteCardsController(StarkitContext db, UserManager<User> userManager, IHostEnvironment environment, UploadService uploadService)
         {
             _db = db;
             _userManager = userManager;
+            _environment = environment;
+            _uploadService = uploadService;
         }
 
         public async Task<IActionResult> Index()
@@ -33,7 +41,7 @@ namespace Starkit.Controllers
         }
 
         [Authorize]
-        public async Task<ActionResult> SaveCarouselData(string dishNameCarousel1, string dishNameCarousel2 , string dishNameCarousel3, string dishTextCarousel1,string dishTextCarousel2, string dishTextCarousel3)
+        public async Task<ActionResult> SaveCarouselData(string dishNameCarousel1, string dishNameCarousel2 , string dishNameCarousel3, string dishTextCarousel1,string dishTextCarousel2, string dishTextCarousel3,IFormFile file1, IFormFile file2, IFormFile file3)
         {
             var userId = _userManager.GetUserId(User);
             var restaurant = await _db.Restaurants.FirstOrDefaultAsync(r => r.UserId == userId);
@@ -47,9 +55,23 @@ namespace Starkit.Controllers
             siteData.DishTextCarousel2 = dishTextCarousel2;
             siteData.DishTextCarousel3 = dishTextCarousel3;
 
+            if (file1 != null)
+                siteData.ImgPathCarousel1 = await CreateFile(file1);
+            if (file2 != null)
+                siteData.ImgPathCarousel2 = await CreateFile(file2);
+            if (file3 != null)
+                siteData.ImgPathCarousel3 = await CreateFile(file3);
+            
             _db.DataSiteCards.Update(siteData);
             await _db.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+
+        private async Task<string> CreateFile(IFormFile file)
+        {
+            string directoryPath = Path.Combine(_environment.ContentRootPath,"wwwroot/images/restaurants/Carousel");
+            await _uploadService.Upload(directoryPath,file.FileName,file);
+            return $"images/restaurants/Carousel/{file.FileName}";
         }
       
     }
