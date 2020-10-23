0d2b357d55b518c38cb3c866636d0f62a996647e
Samal <samal.zhex@gmail.com>
2020-10-23 17:46:52 +0600

    #90 Добавила валидацию ввода даты и времени в бронирование

--- Starkit/ViewModels/CreateBookingViewModel.cs
+++ Starkit/ViewModels/CreateBookingViewModel.cs
@@ -9,7 +9,9 @@ namespace Starkit.ViewModels
     {
         public int TableId { get; set; }
         public string Date { get; set; }
+        [Required(ErrorMessage = "Выберите время")]
         public string BookFrom { get; set; }
+        [Required(ErrorMessage = "Выберите время")]
         public string BookTo { get; set; }
         [Required(ErrorMessage = "Это поле обязательно для заполнения")]
         [MinLength(2, ErrorMessage = "Минимальное количество символов 2")]
@@ -24,6 +26,7 @@ namespace Starkit.ViewModels
         [DataType(DataType.EmailAddress, ErrorMessage = "Введите корректный email")]
         public string Email { get; set; }
         public string Comment { get; set; }
+        [Required(ErrorMessage = "Выберите дату")]
         public string CustomDate { get; set; }
         
     }
